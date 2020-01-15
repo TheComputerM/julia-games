90c8d6219fb86effd99c4772d6f4c50749af4197
TheComputerM <mudit.harshit99@outlook.com>
2020-01-15 20:18:00 +0530

    Added snake

---
+++ snake/README.md
@@ -0,0 +1,11 @@
+## Snake
+Run the app.jl inside the julia repl.
+```
+julia> include("./app.jl")
+```
+
+Dependencies are `Blink.jl`
+
+__Sample Output__
+
+![Snake](./snake.gif)
---
+++ snake/app.jl
@@ -0,0 +1,130 @@
+using Blink
+
+w = Window(async=false)
+title(w, "Snake")
+
+# Open developer tools
+opentools(w)
+
+body!(w, """<canvas width="400" height="400" id="game"></canvas>""", async=false)
+load!(w, "./style.css", async=false)
+
+canvasID = "game"
+snakeColor = "blue"
+foodColor = "red"
+
+js(w, Blink.JSString("""
+var canvas = document.getElementById("$canvasID");
+var context = canvas.getContext("2d");
+
+var grid = 16;
+var count = 0;
+
+var snake = {
+  x: 160,
+  y: 160,
+
+  dx: grid,
+  dy: 0,
+
+  cells: [],
+
+  maxCells: 4
+};
+var apple = {
+  x: 320,
+  y: 320
+};
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function loop() {
+  requestAnimationFrame(loop);
+
+  if (++count < 4) {
+    return;
+  }
+
+  count = 0;
+  context.clearRect(0,0,canvas.width,canvas.height);
+
+  snake.x += snake.dx;
+  snake.y += snake.dy;
+
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  }
+  else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
+
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  }
+  else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
+  snake.cells.unshift({x: snake.x, y: snake.y});
+
+  if (snake.cells.length > snake.maxCells) {
+    snake.cells.pop();
+  }
+
+  context.fillStyle = "$foodColor";
+  context.fillRect(apple.x, apple.y, grid-1, grid-1);
+
+  context.fillStyle = "$snakeColor";
+  snake.cells.forEach(function(cell, index) {
+
+    context.fillRect(cell.x, cell.y, grid-1, grid-1);
+
+    if (cell.x === apple.x && cell.y === apple.y) {
+      snake.maxCells++;
+
+      apple.x = getRandomInt(0, 25) * grid;
+      apple.y = getRandomInt(0, 25) * grid;
+    }
+
+    for (var i = index + 1; i < snake.cells.length; i++) {
+
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+      }
+    }
+  });
+}
+
+// Adding event listeners
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37 && snake.dx === 0) {
+    snake.dx = -grid;
+    snake.dy = 0;
+  }
+  else if (e.which === 38 && snake.dy === 0) {
+    snake.dy = -grid;
+    snake.dx = 0;
+  }
+  else if (e.which === 39 && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
+  }
+  else if (e.which === 40 && snake.dy === 0) {
+    snake.dy = grid;
+    snake.dx = 0;
+  }
+});
+
+requestAnimationFrame(loop);
+"""))
